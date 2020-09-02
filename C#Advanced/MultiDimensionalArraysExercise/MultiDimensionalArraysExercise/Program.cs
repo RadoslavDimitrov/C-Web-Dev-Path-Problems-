8396d8f73986aa2445b4e3adea0e86222bc22767
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-02 23:15:20 +0300

    Matrix and jagged arrays exerscise 1-st done

---
+++ C#Advanced/MultiDimensionalArraysExercise/MultiDimensionalArraysExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Resources;
+
+namespace MultiDimensionalArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine()); //square matrix
+
+            int[,] matrix = new int[rows, rows];
+
+            int col = rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] currArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int colum = 0; colum < rows; colum++)
+                {
+                    matrix[i, colum] = currArr[colum];
+                }
+            }
+
+            int firstSum = 0;
+
+            for (int currRow = 0; currRow < rows; currRow++)
+            {
+                firstSum += matrix[currRow, currRow];
+            }
+
+            int secondSum = 0;
+
+            for (int currRowTwo = 0; currRowTwo < rows; currRowTwo++)
+            {
+                secondSum += matrix[currRowTwo, rows - 1 - currRowTwo];
+            }
+
+            Console.WriteLine(Math.Abs(firstSum - secondSum));
+        }
+    }
+}
