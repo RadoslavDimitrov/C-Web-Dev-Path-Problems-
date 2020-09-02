151044a36fce26c4406d57b94ca608b5c6bc564b
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-02 23:34:58 +0300

    2-nd Done

--- C#Advanced/MultiDimensionalArraysExercise/2. Squares in Matrix/Program.cs
+++ C#Advanced/MultiDimensionalArraysExercise/2. Squares in Matrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _2._Squares_in_Matrix
 {
@@ -6,7 +7,43 @@ namespace _2._Squares_in_Matrix
     {
         static void Main(string[] args)
         {
-            
+            int[] size = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            char[,] matrix = new char[size[0], size[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] currChars = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currChars[col];
+                }
+            }
+
+            char currChar = '0';
+
+
+            int equalSquare = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    currChar = matrix[row, col];
+
+                    if(currChar == matrix[row, col +1] 
+                        && currChar == matrix[row + 1, col]
+                        && currChar == matrix[row + 1, col + 1])
+                    {
+                        equalSquare++;
+                    }
+                }
+            }
+
+            Console.WriteLine(equalSquare);
         }
     }
 }
