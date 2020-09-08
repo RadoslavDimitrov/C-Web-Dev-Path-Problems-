04975d3e704e0e06c394575a75092d0606989653
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-08 23:02:37 +0300

    6-th edited

--- C#Advanced/Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Program.cs
+++ C#Advanced/Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Program.cs
@@ -19,53 +19,64 @@ namespace _06._Wardrobe
 
                 string[] dresses = currInput[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
 
-                if (!wardrobe.ContainsKey(color))
-                {
-                    wardrobe.Add(color, new Dictionary<string, int>());
-
-                    for (int currDress = 0; currDress < dresses.Length; currDress++)
-                    {
-                        if (!wardrobe[color].ContainsKey(dresses[currDress]))
-                        {
-                            wardrobe[color].Add(dresses[currDress], 1);
-                        }
-                        else
-                        {
-                            wardrobe[color][dresses[currDress]]++;
-                        }
-                    }
-
-                }
-                else
-                {
-                    for (int currDress = 0; currDress < dresses.Length; currDress++)
-                    {
-                        if (!wardrobe[color].ContainsKey(dresses[currDress]))
-                        {
-                            wardrobe[color].Add(dresses[currDress], 1);
-                        }
-                        else
-                        {
-                            wardrobe[color][dresses[currDress]]++;
-                        }
-                    }
-                }
+                FillWardrobe(wardrobe, color, dresses);
             }
 
             string[] lookinFor = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
             string lookColor = lookinFor[0];
+
             string lookDress = lookinFor[1];
 
+            PrintWardrobe(wardrobe, lookColor, lookDress);
+        }
+
+        private static void FillWardrobe(Dictionary<string, Dictionary<string, int>> wardrobe, string color, string[] dresses)
+        {
+            if (!wardrobe.ContainsKey(color))
+            {
+                wardrobe.Add(color, new Dictionary<string, int>());
+
+                for (int currDress = 0; currDress < dresses.Length; currDress++)
+                {
+                    if (!wardrobe[color].ContainsKey(dresses[currDress]))
+                    {
+                        wardrobe[color].Add(dresses[currDress], 1);
+                    }
+                    else
+                    {
+                        wardrobe[color][dresses[currDress]]++;
+                    }
+                }
+
+            }
+            else
+            {
+                for (int currDress = 0; currDress < dresses.Length; currDress++)
+                {
+                    if (!wardrobe[color].ContainsKey(dresses[currDress]))
+                    {
+                        wardrobe[color].Add(dresses[currDress], 1);
+                    }
+                    else
+                    {
+                        wardrobe[color][dresses[currDress]]++;
+                    }
+                }
+            }
+        }
+
+        private static void PrintWardrobe(Dictionary<string, Dictionary<string, int>> wardrobe, string lookColor, string lookDress)
+        {
             foreach (var item in wardrobe)
             {
-                if(item.Key == lookColor)
+                if (item.Key == lookColor)
                 {
                     Console.WriteLine($"{item.Key} clothes:");
 
                     foreach (var cloth in item.Value)
                     {
-                        if(cloth.Key == lookDress)
+                        if (cloth.Key == lookDress)
                         {
                             Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
                         }
@@ -73,7 +84,7 @@ namespace _06._Wardrobe
                         {
                             Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
                         }
-                        
+
                     }
                 }
                 else
@@ -82,8 +93,8 @@ namespace _06._Wardrobe
 
                     foreach (var cloth in item.Value)
                     {
-                        
-                            Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
 
                     }
                 }
