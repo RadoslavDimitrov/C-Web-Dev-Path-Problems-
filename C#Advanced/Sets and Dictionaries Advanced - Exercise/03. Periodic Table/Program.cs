84d942b8f6998ef06016d66c435732f40cc303f0
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-08 21:55:02 +0300

    3-rd done, added problem descriptions

---
+++ C#Advanced/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            SortedSet<string> myTable = new SortedSet<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int currInput = 0; currInput < input.Length; currInput++)
+                {
+                    myTable.Add(input[currInput]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",myTable));
+        }
+    }
+}
