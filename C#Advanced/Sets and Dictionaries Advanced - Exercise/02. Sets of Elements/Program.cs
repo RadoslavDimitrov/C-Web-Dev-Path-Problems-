c63843678de8b3e18d04e1542ec3f32bdf91afae
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-08 21:49:22 +0300

    1 and 2-nd Done

---
+++ C#Advanced/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstAndSecondSetLength = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstLength = firstAndSecondSetLength[0];
+            int secondLength = firstAndSecondSetLength[1];
+
+            HashSet<string> firstSet = new HashSet<string>();
+            HashSet<string> secondSet = new HashSet<string>();
+
+            string input;
+
+            for (int i = 0; i < firstLength; i++)
+            {
+                input = Console.ReadLine();
+                firstSet.Add(input);
+            }
+
+            for (int i = 0; i < secondLength; i++)
+            {
+                input = Console.ReadLine();
+                secondSet.Add(input);
+            }
+
+            firstSet.IntersectWith(secondSet);
+
+            Console.WriteLine(string.Join(" ", firstSet));
+        }
+    }
+}
---
+++ C#Advanced/Sets and Dictionaries Advanced - Exercise/Sets and Dictionaries Advanced - Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets_and_Dictionaries_Advanced___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfInputs = int.Parse(Console.ReadLine());
+
+            HashSet<string> mySet = new HashSet<string>();
+
+            for (int i = 0; i < numOfInputs; i++)
+            {
+                string input = Console.ReadLine();
+
+                mySet.Add(input);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, mySet));
+        }
+    }
+}
