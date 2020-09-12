f9c55efb0d49f87cc3f1577c9eaa4c337e01f363
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-12 23:27:05 +0300

    3-rd problem done

---
+++ C#Advanced/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> returnSmallest = num =>
+            {
+
+                int min = int.MaxValue;
+                for (int i = 0; i < num.Length; i++)
+                {
+                    if (num[i] < min)
+                    {
+                        min = num[i];
+                    }
+                }
+                
+
+                return min;
+            };
+
+            var nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+
+            int num = returnSmallest(nums.ToArray());
+
+            Console.WriteLine(num);
+        }
+    }
+}
