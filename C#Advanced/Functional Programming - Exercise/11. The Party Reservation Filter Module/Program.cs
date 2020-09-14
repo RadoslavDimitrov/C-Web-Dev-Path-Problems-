f1171edaee2294772eb74c8e265b2ae75640b41a
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-15 01:24:03 +0300

    11-th almost done

---
+++ C#Advanced/Functional Programming - Exercise/11. The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _11._The_Party_Reservation_Filter_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read input
+
+            //make while != Print
+                    //split command
+                    //switch or for loop for command[0]
+
+        }
+
+        public Predicate<string> GetPredicate(string filter, string argument)
+        {
+            switch (filter)
+            {
+                case "Starts with":
+                    return name => name.StartsWith(argument);
+                case "Ends sith":
+                    return name => name.EndsWith(argument);
+                case "Length":
+                    return name => name.Length == int.Parse(argument);
+                case "Contains":
+                    return name => name.Contains(argument);
+                default:
+                    throw new ArgumentException("Invalid command " + filter);
+                    break;
+            }
+        }
+    }
+}
