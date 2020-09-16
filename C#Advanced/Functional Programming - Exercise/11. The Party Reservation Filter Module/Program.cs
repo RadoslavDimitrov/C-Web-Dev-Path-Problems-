87e916c1035fa15e27799092c4d66acd7aa50361
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-16 23:40:54 +0300

    till 12-th problem done

--- C#Advanced/Functional Programming - Exercise/11. The Party Reservation Filter Module/Program.cs
+++ C#Advanced/Functional Programming - Exercise/11. The Party Reservation Filter Module/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11._The_Party_Reservation_Filter_Module
 {
@@ -7,29 +9,61 @@ namespace _11._The_Party_Reservation_Filter_Module
         static void Main(string[] args)
         {
             //read input
+            List<string> input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<string> filters = new List<string>();
+
 
             //make while != Print
-                    //split command
-                    //switch or for loop for command[0]
 
-        }
+            string command = Console.ReadLine();
 
-        public Predicate<string> GetPredicate(string filter, string argument)
-        {
-            switch (filter)
+            while (command != "Print")
             {
-                case "Starts with":
-                    return name => name.StartsWith(argument);
-                case "Ends sith":
-                    return name => name.EndsWith(argument);
-                case "Length":
-                    return name => name.Length == int.Parse(argument);
-                case "Contains":
-                    return name => name.Contains(argument);
-                default:
-                    throw new ArgumentException("Invalid command " + filter);
-                    break;
+                string[] currCommand = command.Split(new char[] {';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                switch (currCommand[0])
+                {
+                    case "Add filter":
+                        filters.Add(currCommand[1] + " " + currCommand[2]);
+                        break;
+                    case "Remove filter":
+                        filters.Remove(currCommand[1] + " " + currCommand[2]);
+                        break;
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var filter in filters)
+            {
+                var commands = filter.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (commands[0])
+                {
+                    case "Starts":
+                        input = input.Where(x => !x.StartsWith(commands[2])).ToList();
+                        break;
+                    case "Ends":
+                        input = input.Where(x => !x.EndsWith(commands[2])).ToList();
+                        break;
+                    case "Length":
+                        input = input.Where(x => x.Length != int.Parse(commands[1])).ToList();
+                        break;
+                    case "Contains":
+                        input = input.Where(x => !x.Contains(commands[1])).ToList();
+                        break;
+
+                }
+            }
+
+            if (input.Any())
+            {
+                Console.WriteLine(string.Join(" ", input));
             }
         }
+
+        
     }
 }
---
+++ C#Advanced/Functional Programming - Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<string> input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, int, bool> isGreaterSum = (name, num) => name.ToCharArray()
+            .Select(ch => (int)ch).Sum() >= num;
+
+            Func<List<string>, int, Func<string, int, bool>, string> combineName = (names, num, func) =>
+            names.FirstOrDefault(str => func(str, num));
+
+            var result = combineName(input, number, isGreaterSum);
+            Console.WriteLine(result);
+            
+
+            
+            
+        }
+    }
+}
