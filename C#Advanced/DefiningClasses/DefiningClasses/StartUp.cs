9fdc0ce4791cdd304a31b28cb405565fb0038605
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-18 21:18:47 +0300

    Define a Class Person

---
+++ C#Advanced/DefiningClasses/DefiningClasses/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Age = 18;
+            person.Name = "Pesho";
+        }
+    }
+}
