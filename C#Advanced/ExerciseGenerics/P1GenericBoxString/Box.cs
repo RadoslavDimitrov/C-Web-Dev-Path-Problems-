bb183b81e2b536e0d26dedc5df903c42752a32a7
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-29 22:46:25 +0300

    P1 done

---
+++ C#Advanced/ExerciseGenerics/P1GenericBoxString/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1GenericBoxОfString
+{
+    class Box<T>
+    {
+        private T data;
+
+        public Box(T data)
+        {
+            this.data = data;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T).FullName}: {this.data}";
+            //return "{class full name: value}"
+        }
+    }
+}
---
+++ C#Advanced/ExerciseGenerics/P1GenericBoxString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P1GenericBoxОfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string currStr = Console.ReadLine();
+
+                var myBox = new Box<string>(currStr);
+
+                Console.WriteLine(myBox.ToString());
+            }
+        }
+    }
+}
