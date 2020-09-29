8dabcd3629bfb44e54587b8cbd43b318f2efabb2
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-09-30 02:23:50 +0300

    Update to Problem 8

---
+++ C#Advanced/ExerciseGenerics/P7.Tuple/MyTuple.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P7.Tuple
+{
+    public class MyTuple<T, V>
+    {
+        private T item1;
+
+        private V item2;
+
+        public V Item2
+        {
+            get { return item2; }
+            set { item2 = value; }
+        }
+
+        public T Item1
+        {
+            get { return item1; }
+            set { item1 = value; }
+        }
+
+       
+
+        public MyTuple(T item1, V item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+    }
+}
---
+++ C#Advanced/ExerciseGenerics/P7.Tuple/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Loader;
+
+namespace P7.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringInput = Console.ReadLine().Split();
+            string fullName = $"{stringInput[0]} {stringInput[1]}";
+            MyTuple<string, string> stringTuple = new MyTuple<string, string>(fullName, stringInput[2]);
+            Console.WriteLine(stringTuple);
+
+            string[] secondInput = Console.ReadLine().Split();
+            MyTuple<string, int> secondTuple = new MyTuple<string, int>(secondInput[0], int.Parse(secondInput[1]));
+            Console.WriteLine(secondTuple);
+
+            string[] numsInput = Console.ReadLine().Split();
+            MyTuple<int, double> numTuple = new MyTuple<int, double>(int.Parse(numsInput[0]), double.Parse(numsInput[1]));
+            Console.WriteLine(numTuple);
+        }
+    }
+}
