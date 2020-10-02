7ffd47a0790c9cbbb0a32fb84c88150199d90b35
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-10-03 01:00:31 +0300

    problem 6 done

--- C#Advanced/ExerciseStreamsFilesAndDirectories/P6.ZipAndExtract/Program.cs
+++ C#Advanced/ExerciseStreamsFilesAndDirectories/P6.ZipAndExtract/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 
-namespace P6.ZipAndExtract
+namespace ZipAndExtract
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string filePath = @".\copyMe.png";
+
+            string zipPath = @"C:\Users\morve\OneDrive\Desktop\Newfolder";
+
+            string ExtractedFile = @"C:\Users\morve\OneDrive\Desktop\Newfolder\output";
+
             
+            ZipFile.CreateFromDirectory(zipPath, filePath);
+            ZipFile.ExtractToDirectory(zipPath, ExtractedFile);
         }
     }
 }
