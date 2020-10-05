9caa42f3ad93a084db01dfae5ad7dbabe71bcd87
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-10-05 23:24:34 +0300

    P1 done

---
+++ C#Advanced/ExerciseIteratorsAndComparators/ExerciseIteratorsAndComparators/ListyIterator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ExerciseIteratorsAndComparators
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private readonly List<T> elements;
+        private int index;
+
+        public ListyIterator()
+        {
+            this.elements = new List<T>();
+            this.index = 0;
+        }
+
+        public ListyIterator(IEnumerable<T> elements)
+        {
+            this.elements = new List<T>(elements);
+        }
+        //public void Create(List<T> elements)
+        //{
+
+        //}
+
+        public bool Move()
+        {
+            this.index++;
+            if( this.index < this.elements.Count)
+            {
+                return true;
+            }
+            else
+            {
+                index--;
+                return false;
+            }
+        }
+
+        public void Print()
+        {
+            //if (this.index < this.elements.Count)
+            //{
+            //    Console.WriteLine(this.elements[this.index]);
+            //}
+            //else
+            //{
+
+            //    throw new ArgumentException("Invalid Operation!");
+            //}
+
+            try
+            {
+
+                Console.WriteLine(this.elements[this.index]);
+
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+
+        }
+
+        public bool HasNext()
+        {
+            if (this.index + 1 < this.elements.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.elements)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#Advanced/ExerciseIteratorsAndComparators/ExerciseIteratorsAndComparators/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ExerciseIteratorsAndComparators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            ListyIterator<string> mylist = new ListyIterator<string>();
+
+            while (command != "END")
+            {
+                var currCommand = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command.Contains("Create"))
+                {
+                    
+
+                    var currArr = new List<string>();
+
+                    if(currCommand.Length > 0)
+                    {
+                        for (int i = 1; i < currCommand.Length; i++)
+                        {
+                            currArr.Add(currCommand[i]);
+                        }
+                    }
+                    
+                    mylist = new ListyIterator<string>(currArr);
+                }
+                else if (command.Contains("Move"))
+                {
+                    Console.WriteLine(mylist.Move().ToString());
+                }
+                else if (command.Contains("HasNext"))
+                {
+                    Console.WriteLine(mylist.HasNext().ToString());
+                }
+                else if (currCommand[0] == "Print")
+                {
+                    mylist.Print();
+                }
+                else if (currCommand[0] == "PrintAll")
+                {
+                    foreach (var item in mylist)
+                    {
+                        Console.Write(item + " ");
+                    }
+
+                    Console.WriteLine();
+                }
+
+
+
+                command = Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
