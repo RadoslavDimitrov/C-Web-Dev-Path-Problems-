4191c0556614b4774c6e849486852ae2ebabc674
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-10-06 00:23:13 +0300

    P2 done

---
+++ C#Advanced/ExerciseIteratorsAndComparators/P2.Stack/MyStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2.Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        public int Count { get; private set; }
+
+        public MyStack()
+        {
+            this.elements = new List<T>();
+            this.Count = 0;
+        }
+        public void Push(T[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                this.elements.Add(elements[i]);
+                this.Count++;
+            }
+        }
+
+        public T Pop()
+        {
+            T currEle = this.elements[this.elements.Count - 1];
+            this.elements.RemoveAt(this.elements.Count - 1);
+            this.Count--;
+            return currEle;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.elements.Count - 1; i >= 0; i--)
+            {
+                yield return this.elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#Advanced/ExerciseIteratorsAndComparators/P2.Stack/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P2.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new MyStack<int>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (command.Contains("Pop"))
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch(ArgumentException ae)
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                }
+                else //push command
+                {
+                    string arrWithCommand = command.Remove(0, 5);
+                    int[] arr = arrWithCommand.Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                    stack.Push(arr);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+            
+        }
+    }
+}
