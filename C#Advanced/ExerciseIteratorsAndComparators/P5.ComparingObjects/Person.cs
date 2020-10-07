dedd4448643823c2290f5c1a8636dbab0327e16a
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-10-07 22:07:16 +0300

    P5 done

---
+++ C#Advanced/ExerciseIteratorsAndComparators/P5.ComparingObjects/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace P5.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public Person()
+        {
+            this.age = 0; 
+        }
+
+        public Person(string name, int age, string town) : this()
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            if(this.name.CompareTo(other.name) == 0) //even name
+            {
+                if(this.age.CompareTo(other.age) == 0) //even age
+                {
+                    if(this.town.CompareTo(other.town) == 0) //even town
+                    {
+                        return 0;
+                    }
+                    else if(this.town.CompareTo(other.town) < 0)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                else if(this.age.CompareTo(other.age) < 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else if(this.name.CompareTo(other.name) < 0) //is smaller
+            {
+                return -1;
+            }
+            else // is bigger
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ C#Advanced/ExerciseIteratorsAndComparators/P5.ComparingObjects/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P5.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var personData = new List<Person>();
+
+            while (input != "END")
+            {
+                string[] currInput = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string currName = currInput[0];
+                int currAge = int.Parse(currInput[1]);
+                string currCity = currInput[2];
+
+                var currPerson = new Person(currName, currAge, currCity);
+
+                personData.Add(currPerson);
+
+                input = Console.ReadLine();
+            }
+
+            int numOfPersonToCompare = int.Parse(Console.ReadLine()) - 1;
+
+            int equalPersons = 0;
+            int notEqualPersons = 0;
+
+            for (int i = 0; i < personData.Count; i++)
+            {
+                if (personData[numOfPersonToCompare].CompareTo(personData[i]) == 0)
+                {
+                    equalPersons++;
+                }
+                else
+                {
+                    notEqualPersons++;
+                }
+            }
+
+            if(equalPersons == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equalPersons} {notEqualPersons} {personData.Count}");
+            }
+            
+
+            //Console.WriteLine(p1.CompareTo(p3)); // 1 - p1 is bigger than p3
+        }
+    }
+}
