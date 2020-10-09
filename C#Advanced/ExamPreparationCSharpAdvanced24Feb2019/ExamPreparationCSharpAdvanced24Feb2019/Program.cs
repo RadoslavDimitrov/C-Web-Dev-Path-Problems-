003d6bd9c80a646a0f8d21a05873f7332fa02598
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-10-10 00:19:29 +0300

    P1 done

---
+++ C#Advanced/ExamPreparationCSharpAdvanced24Feb2019/ExamPreparationCSharpAdvanced24Feb2019/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamPreparationCSharpAdvanced24Feb2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hallCapacity = int.Parse(Console.ReadLine());
+
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<string> stackInput = new Stack<string>(input);
+
+            Queue<char> openedHalls = new Queue<char>();
+            List<int> currResSum = new List<int>();
+
+            while (stackInput.Count > 0)
+            {
+                var currPop = stackInput.Pop();
+                var currPopAsCharArr = currPop.ToCharArray();
+
+                if (char.IsLetter(currPopAsCharArr[0]))
+                {
+                    openedHalls.Enqueue(currPopAsCharArr[0]);
+                    continue;
+                }
+
+                if(openedHalls.Count == 0)
+                {
+                    continue;
+                }
+
+                int currReservation = int.Parse(currPop);
+
+                if(currResSum.Sum() + currReservation <= hallCapacity)
+                {
+                    currResSum.Add(currReservation);
+                }
+                else
+                {
+                    Console.WriteLine($"{openedHalls.Dequeue()} -> " + string.Join(", ", currResSum));
+                    currResSum.Clear();
+
+                    if (openedHalls.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    if (currReservation > hallCapacity)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        currResSum.Add(currReservation);
+                    }
+                }
+            }
+        }
+    }
+}
