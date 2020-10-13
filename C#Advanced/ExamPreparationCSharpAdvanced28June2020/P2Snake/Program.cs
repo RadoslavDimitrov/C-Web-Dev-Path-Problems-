71375c14ab7089e6453f4fdcff7d0eb55b00e3ca
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-10-13 23:12:11 +0300

    P2 started

--- C#Advanced/ExamPreparationCSharpAdvanced28June2020/P2Snake/Program.cs
+++ C#Advanced/ExamPreparationCSharpAdvanced28June2020/P2Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace P2Snake
 {
@@ -8,7 +9,38 @@ namespace P2Snake
         {
             int size = int.Parse(Console.ReadLine());
 
-            str
+            string[,] matrix = new string[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string currRow = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                     matrix[row, col] = currRow[col].ToString();
+                }
+            }
+        }
+
+        public static KeyValuePair<int, int> GetPosition(string[,] matrix, string charToLook)
+        {
+            KeyValuePair<int, int> currPair = new KeyValuePair<int, int>( -1, -1);
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == charToLook)
+                    {
+                        currPair = new KeyValuePair<int, int>(row, col);
+                        return currPair;
+                    }
+                }
+            }
+
+            return currPair;
         }
     }
+
+    
 }
