a302f9a7debf3be4a2d5113d83be199008c8063e
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-10-15 22:30:32 +0300

    P1 done and P2 started

---
+++ C#Advanced/CSHarpAdvancedExam-22Feb2020/CSHarpAdvancedExam-22Feb2020/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSHarpAdvancedExam_22Feb2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int[] secondNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            Stack<int> secondBox = new Stack<int>(secondNumbers);
+            Queue<int> firstBox = new Queue<int>(firstNumbers);
+
+            List<int> myLoot = new List<int>();
+
+            while (true)
+            {
+                if(firstBox.Count == 0)
+                {
+                    break;
+                }
+                if(secondBox.Count == 0)
+                {
+                    break;
+                }
+                int firstNum = firstBox.Peek();
+                int secondNum = secondBox.Peek();
+                int result = firstNum + secondNum;
+                if(result % 2 == 0)
+                {
+                    myLoot.Add(result);
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    int temp = secondBox.Pop();
+                    firstBox.Enqueue(temp);
+                }
+            }
+
+            if(firstBox.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            int itemValue = myLoot.Sum();
+
+            if(itemValue >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {itemValue}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {itemValue}");
+            }
+        }
+    }
+}
---
+++ C#Advanced/CSHarpAdvancedExam-22Feb2020/P2Re-Volt/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P2Re_Volt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int countOfCommands = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[size, size];
+            FillTheMatrix(matrix);
+
+            //"f" - player
+            //"-" - empty
+            //"B" - bonus - one move forward
+            //"T" - trap - one move backward
+            //"F" - finish
+            KeyValuePair<int, int> playerPos = GetPosition(matrix, "f");
+
+            for (int i = 0; i < countOfCommands; i++)
+            {
+                string currCommand = Console.ReadLine().ToLower();
+
+                switch (currCommand)
+                {
+                    case "up":
+                        if(playerPos.Key == 0)
+                        {
+                            matrix[playerPos.Key, playerPos.Value] = "-";
+                            playerPos = new KeyValuePair<int, int>(matrix.GetLength(0) - 1, playerPos.Value);
+                            matrix[playerPos.Key, playerPos.Value] = "f";
+                        }
+                        else if (matrix[playerPos.Key - 1, playerPos.Value] == "B")
+                        {
+
+                        }
+                        break;
+                    case "down":
+                        break;
+                    case "left":
+                        break;
+                    case "right":
+                        break;
+                    default:
+                        throw new Exception("Wrong move command");
+
+                }
+            }
+        }
+
+        public static KeyValuePair<int, int> Move(string[,] matrix,
+            KeyValuePair<int,int> playerPos,
+            KeyValuePair<int, int> directions)
+        {
+
+        }
+
+        private static KeyValuePair<int, int> GetPosition(string[,] matrix, string v)
+        {
+            KeyValuePair<int, int> currPos = new KeyValuePair<int, int>(-1, -1);
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if(matrix[row,col] == v)
+                    {
+                        currPos = new KeyValuePair<int, int>(row, col);
+                        return currPos;
+                    }
+                }
+            }
+
+            return currPos;
+        }
+
+        private static void FillTheMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string currRow = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col].ToString();
+                }
+            }
+        }
+    }
+}
