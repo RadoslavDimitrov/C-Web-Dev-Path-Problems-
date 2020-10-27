081f2de74163e85d132ee15c318ea3cd53bc8ec9
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-10-27 22:45:26 +0200

    P1 from Encapsulation done

---
+++ C#OOP/EncapsulationExercise/EncapsulationExercise/Box.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationExercise
+{
+    public class Box
+    {
+		private double length;
+
+		private double width;
+
+		private double height;
+
+		public Box(double length, double width, double height)
+		{
+			this.Length = length;
+			this.Width = width;
+			this.Height = height;
+		}
+		public double Height
+		{
+			get { return height; }
+			private set 
+			{ 
+				if(value <= 0)
+				{
+					//throw new ArgumentException("Height cannot be zero or negative.");
+					Console.WriteLine("Height cannot be zero or negative.");
+				}
+				else
+				{
+					height = value;
+				}
+				
+			}
+		}
+
+		public double Width
+		{
+			get { return width; }
+			private set 
+			{
+				if (value <= 0)
+				{
+					//throw new ArgumentException("Width cannot be zero or negative.");
+					Console.WriteLine("Width cannot be zero or negative.");
+				}
+				else
+				{
+					width = value;
+				}
+
+			}
+		}
+
+
+		public double Length
+		{
+			get { return length; }
+			private set 
+			{
+				if (value <= 0)
+				{
+					//throw new ArgumentException("Length cannot be zero or negative.");
+					Console.WriteLine("Length cannot be zero or negative.");
+				}
+				else
+				{
+					length = value;
+				}
+
+			}
+		}
+
+		public double SurfaceArea()
+		{
+			//2lw + 2lh + 2wh
+			return 2 * this.length * this.width + 2 * this.length * this.height + 2 * this.width * this.height;
+		}
+
+		public double LateralSurfaceArea()
+		{
+			// 2lh + 2wh
+			return 2 * this.length * this.height + 2 * this.width * this.height;
+		}
+
+		public double Volume()
+		{
+			//lwh
+			return this.length * this.width * this.height;
+		}
+	}
+}
---
+++ C#OOP/EncapsulationExercise/EncapsulationExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml;
+
+namespace EncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            double boxSA = box.SurfaceArea();
+            double boxLSA = box.LateralSurfaceArea();
+            double boxVolume = box.Volume();
+
+            if(boxSA > 0 && boxLSA > 0 && boxVolume > 0)
+            {
+                Console.WriteLine($"Surface Area - {boxSA:F2}");
+                Console.WriteLine($"Lateral Surface Area - {boxLSA:F2}");
+                Console.WriteLine($"Volume - {boxVolume:F2}");
+            }
+
+        }
+    }
+}
