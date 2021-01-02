faf660c06602b6a09e5ec551d76b47fbe0c9f006
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-01-03 00:03:23 +0200

    DB TableRelation

---
+++ C#DataBase/MSSQLServer/TableRelations/OneToOneRelation.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE PersonIndentifier
+
+USE PersonIndentifier
+
+CREATE TABLE Passports
+(
+	PassportID INT PRIMARY KEY NOT NULL,
+	PassportNumber NVARCHAR(20) NOT NULL
+)
+
+CREATE TABLE Persons
+(
+	PersonID INT PRIMARY KEY IDENTITY NOT NULL,
+	FirstName NVARCHAR(20) NOT NULL,
+	Salary DECIMAL(8,2) NOT NULL,
+	PassportID INT FOREIGN KEY REFERENCES Passports(PassportID)
+)
+
+INSERT INTO Passports
+	VALUES
+	(101, 'N34FG21B' ),
+	(102, 'K65LO4R7'),
+	(103, 'ZE657QP2')
+
+INSERT INTO Persons
+	VALUES
+	('Roberto', 43300.00, 102),
+	('Tom',56100.00, 103),
+	('Yana', 60200.00, 101)
