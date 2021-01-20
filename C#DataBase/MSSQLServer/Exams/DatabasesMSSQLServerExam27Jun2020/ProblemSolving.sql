afb63c8b6a7d82fdc8eb48cfe49f59dc33f377ec
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-01-20 23:36:54 +0200

    Update ProblemSolving.sql

--- C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerExam27Jun2020/ProblemSolving.sql
+++ C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerExam27Jun2020/ProblemSolving.sql
@@ -177,5 +177,65 @@ SELECT P.PartId, P.Description, PN.Quantity as [Requared], P.StockQty as [In Sto
 
 	GROUP BY P.PartId, P.Description, PN.Quantity, P.StockQty, OP.Quantity
 
+--SECTION 4 
 
+--	11.	Place Order
 
+CREATE PROC usp_PlaceOrder(@jobId INT, @partSerial NVARCHAR(50), @quantity INT)
+AS
+
+	IF(@quantity <= 0)
+		THROW 50012, 'Part quantity must be more than zero!', 1;
+
+	IF(@jobId IN (SELECT JobId
+							FROM Jobs
+								WHERE Jobs.[Status] LIKE 'FINISHED'))
+		THROW 50011, 'This job is not active!', 1;
+
+	IF(@partSerial NOT IN (SELECt P.SerialNumber
+							FROM Parts as P))
+		THROW 50014, 'Part not found!', 1;
+
+	IF(@jobId NOT IN (SELECT JobId	
+							FROM Jobs))
+		THROW 50013, 'Job not found!', 1;
+
+	IF(@jobId IN (SELECT JobId 
+						FROM Jobs) AND (SELECT IssueDate
+												FROM Orders
+												WHERE JobId = @jobId) IS NULL)
+			BEGIN TRANSACTION
+			DECLARE @partId INT = (SELECT PartId 
+									FROM Parts as P
+									WHERE P.SerialNumber = @partSerial);
+
+			DECLARE @orderId INT = (SELECT OrderId
+									FROM Orders as O
+									WHERE O.JobId = @jobId AND IssueDate IS NULL);
+
+			IF(@orderId IN (SELECT OrderId
+									FROM Orders) AND
+									@partId IN (SELECT O.PartId
+													FROM OrderParts as O))
+					BEGIN
+						UPDATE OrderParts
+						SET Quantity += @quantity
+						WHERE OrderId = @orderId AND PartId = @partId
+					END
+			ELSE
+					BEGIN
+						INSERT INTO OrderParts(OrderId, PartId, Quantity)
+						VALUES(@orderId, @partId, @quantity)
+					END
+	COMMIT
+GO
+
+DECLARE @err_msg AS NVARCHAR(MAX);
+BEGIN TRY
+    EXEC usp_PlaceOrder 1, 'ZeroQuantity', 0
+END TRY
+BEGIN CATCH
+    SET @err_msg = ERROR_MESSAGE();
+    SELECT @err_msg
+END CATCH
+--Part quantity must be more than zero!
