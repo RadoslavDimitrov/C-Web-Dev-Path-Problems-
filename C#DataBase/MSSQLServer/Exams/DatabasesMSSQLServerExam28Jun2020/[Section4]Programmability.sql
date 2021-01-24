b1e0557476e9ba0a1ac18695edbe6898217e5bc6
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-01-24 19:01:07 +0200

    Update [Section4]Programmability.sql

--- C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerExam28Jun2020/[Section4]Programmability.sql
+++ C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerExam28Jun2020/[Section4]Programmability.sql
@@ -26,5 +26,41 @@ BEGIN
 END
 GO
 
-
 SELECT dbo.udf_GetColonistsCount('Mars') 
+
+
+--Change Journey Purpose
+
+CREATE OR ALTER PROC usp_ChangeJourneyPurpose(@JourneyId INT, @NewPurpose VARCHAR(11))
+AS
+BEGIN
+	BEGIN TRANSACTION
+	IF((SELECT Id
+		FROM Journeys
+		WHERE Id = @JourneyId) IS NULL)
+		BEGIN
+			THROW 50001, 'The journey does not exist!', 1;
+		END
+
+	IF((SELECT TOP(1) Purpose
+		FROM Journeys
+		WHERE Purpose = @NewPurpose AND Id = @JourneyId) IS NOT NULL)
+		BEGIN
+			THROW 50002, 'You cannot change the purpose!', 1;
+		END
+
+	UPDATE Journeys
+		SET Purpose = @NewPurpose
+		WHERE Id = @JourneyId
+		
+COMMIT
+END
+
+EXEC usp_ChangeJourneyPurpose 2, 'Educational' 
+
+--You cannot change the purpose! 
+
+EXEC usp_ChangeJourneyPurpose 196, 'Technical'
+--The journey does not exist!
+
+EXEC usp_ChangeJourneyPurpose 4, 'Technical' 
