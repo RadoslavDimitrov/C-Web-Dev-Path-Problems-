dd119317941be3e0722ec78a7187e26b55626f69
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-01-24 17:21:35 +0200

    Update [Section2]DML.sql

--- C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerExam28Jun2020/[Section2]DML.sql
+++ C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerExam28Jun2020/[Section2]DML.sql
@@ -14,3 +14,11 @@ VALUES
 	('Falcon9','SpaceX',1),
 	('Bed','Vidolov',6)
 
+
+--Update 
+
+--Update all spaceships light speed rate with 1 where the Id is between 8 and 12. 
+
+UPDATE Spaceships
+	SET [LightSpeedRate]+=1
+	WHERE [Id] BETWEEN 8 AND 12
