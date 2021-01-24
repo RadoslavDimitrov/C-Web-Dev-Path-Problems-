38943d3db359aa18d79d5d8a283773b3038cdd33
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-01-24 17:36:07 +0200

    Update [Section3]Querying.sql

--- C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerExam28Jun2020/[Section3]Querying.sql
+++ C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerExam28Jun2020/[Section3]Querying.sql
@@ -6,3 +6,12 @@ SELECT Id,
 	FROM Journeys as j
 	WHERE Purpose IN ('Military')
 	ORDER BY j.JourneyStart ASC
+
+--Select all pilots 
+
+SELECT c.Id,
+	c.FirstName + ' ' + c.LastName as [full_name]
+	FROM Colonists as c
+	JOIN TravelCards as tc ON tc.ColonistId = c.Id
+	WHERE tc.JobDuringJourney IN ('Pilot')
+	ORDER BY c.Id ASC
