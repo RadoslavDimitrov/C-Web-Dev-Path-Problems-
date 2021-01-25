79051cfaf267bf3a91c97cf004d0a340c14f863a
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-01-25 21:01:27 +0200

    Update [Section3]Querying.sql

--- C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerRetakeExam11August2020/[Section3]Querying.sql
+++ C#DataBase/MSSQLServer/Exams/DatabasesMSSQLServerRetakeExam11August2020/[Section3]Querying.sql
@@ -5,3 +5,30 @@ SELECT p.[Name],
 	p.Description
 	FROM Products as p
 	ORDER BY Price DESC, [Name] ASC
+
+
+--Negative Feedback 
+
+		--Filter only feedbacks which have rate below 5.0
+
+SELECT f.ProductId,
+	f.Rate,
+	f.[Description],
+	f.CustomerId,
+	c.Age,
+	c.Gender
+	FROM [Feedbacks] as f
+	JOIN Customers as c ON f.CustomerId = c.Id
+	WHERE f.Rate < 5.0
+	ORDER BY f.ProductId DESC, f.Rate ASC
+
+
+--Customers without Feedback 
+
+SELECT c.FirstName + ' ' + c.LastName as [CustomerName],
+	c.PhoneNumber,
+	c.Gender
+	FROM Customers as c
+	WHERE c.Id NOT IN (SELECT CustomerId
+							FROM Feedbacks)
+		ORDER BY c.Id ASC
