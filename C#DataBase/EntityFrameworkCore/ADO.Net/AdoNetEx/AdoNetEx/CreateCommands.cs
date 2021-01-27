9ce26ce2b161efbf6eeaff16cc84bfe32df78ce8
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-01-27 13:55:23 +0200

    Prob1

---
+++ C#DataBase/EntityFrameworkCore/ADO.Net/AdoNetEx/AdoNetEx/CreateCommands.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoNetEx
+{
+    public static class CreateCommands
+    {
+        public static string CreateDB()
+        {
+            return "CREATE DATABASE MinionsDB";
+        }
+
+        public static string CreateTableCountries()
+        {
+            return "CREATE TABLE Countries " +
+                    "(Id INT PRIMARY KEY IDENTITY," +
+                    "[Name] NVARCHAR(50) NOT NULL)";
+        }
+
+        public static string CreateTableTowns()
+        {
+            return "CREATE TABLE Towns" +
+                    "(Id INT PRIMARY KEY IDENTITY," +
+                    "Name NVARCHAR(50) NOT NULL," +
+                    "CountryCode INT REFERENCES Countries(Id))";
+        }
+
+        public static string CreateTableMinions()
+        {
+            return "CREATE TABLE Minions" +
+                    "(Id INT PRIMARY KEY IDENTITY," +
+                    "Name NVARCHAR(50) NOT NULL," +
+                    "Age INT NOT NULL," +
+                    "TownId INT REFERENCES Towns(Id))";
+        }
+
+        public static string CreateTableEvilnessFactors()
+        {
+            return "CREATE TABLE EvilnessFactors" +
+                    "(Id INT PRIMARY KEY IDENTITY," +
+                    "Name NVARCHAR(50) NOT NULL)";
+        }
+
+        public static string CreateTableVillains()
+        {
+            return "CREATE TABLE Villains" +
+                    "(Id INT PRIMARY KEY IDENTITY," +
+                    "Name NVARCHAR(50) NOT NULL," +
+                    "EvilnessFactorId INT REFERENCES EvilnessFactors(Id))";
+        }
+
+        public static string CreateTableMinionsVillains()
+        {
+            return "CREATE TABLE MinionsVillains" +
+                    "(MinionId INT REFERENCES Minions(Id) NOT NULL," +
+                    "VillainId INT REFERENCES VIllains(Id) NOT NULL," +
+                    "PRIMARY KEY (MinionId, VillainId))";
+        }
+    }
+}
--- C#DataBase/EntityFrameworkCore/ADO.Net/AdoNetEx/AdoNetEx/Program.cs
+++ C#DataBase/EntityFrameworkCore/ADO.Net/AdoNetEx/AdoNetEx/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 
 namespace AdoNetEx
 {
@@ -6,7 +7,51 @@ namespace AdoNetEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connectonString =
+                   @"Server=.\SQLEXPRESS;Database=Master;Integrated Security=true;";
+            SqlConnection sqlConnection = new SqlConnection(connectonString);
+
+            sqlConnection.Open();
+
+            using (sqlConnection)
+            {
+                string com = CreateCommands.CreateDB();
+                SqlCommand command = new SqlCommand(com, sqlConnection);
+                command.ExecuteNonQuery();
+            }
+
+            connectonString = @"Server=.\SQLEXPRESS;Database=MinionsDB;Integrated Security=true;";
+            sqlConnection = new SqlConnection(connectonString);
+
+            sqlConnection.Open();
+
+            using(sqlConnection)
+            {
+                string com = CreateCommands.CreateTableCountries();
+                SqlCommand command = new SqlCommand(com, sqlConnection);
+                command.ExecuteNonQuery();
+
+                com = CreateCommands.CreateTableTowns();
+                command = new SqlCommand(com, sqlConnection);
+                command.ExecuteNonQuery();
+
+                com = CreateCommands.CreateTableMinions();
+                command = new SqlCommand(com, sqlConnection);
+                command.ExecuteNonQuery();
+
+                com = CreateCommands.CreateTableEvilnessFactors();
+                command = new SqlCommand(com, sqlConnection);
+                command.ExecuteNonQuery();
+
+                com = CreateCommands.CreateTableVillains();
+                command = new SqlCommand(com, sqlConnection);
+                command.ExecuteNonQuery();
+
+                com = CreateCommands.CreateTableMinionsVillains();
+                command = new SqlCommand(com, sqlConnection);
+                command.ExecuteNonQuery();
+
+            }
         }
     }
 }
