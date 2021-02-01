03a17ab507db4fd0c987c7f4eefdb775bf759398
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-02-01 23:27:04 +0200

    Update

--- C#DataBase/EntityFrameworkCore/EntityFrameworkIntro/EntityFrameworkIntro/EntityFrameworkIntro/StartUp.cs
+++ C#DataBase/EntityFrameworkCore/EntityFrameworkIntro/EntityFrameworkIntro/EntityFrameworkIntro/StartUp.cs
@@ -6,13 +6,29 @@ using System.Text;
 
 namespace EntityFrameworkIntro
 {
-    class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
             SoftUniContext DBcontext = new SoftUniContext();
 
-            Console.WriteLine(GetEmployeesFullInformation(DBcontext));
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(DBcontext));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var allEmp = context.Employees
+                .Where(x => x.Salary > 50000)
+                .OrderBy(x => x.FirstName);
+
+            foreach (var emp in allEmp)
+            {
+                sb.AppendLine($"{emp.FirstName} - {emp.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
