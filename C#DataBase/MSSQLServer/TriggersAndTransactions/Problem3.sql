0a420de8c650cb854fa1967f4dd8360ef0927fa4
Radoslav Dimitrov <69116643+RadoslavDimitrov@users.noreply.github.com>
2021-02-01 22:17:34 +0200

    Update Problem3.sql

--- C#DataBase/MSSQLServer/TriggersAndTransactions/Problem3.sql
+++ C#DataBase/MSSQLServer/TriggersAndTransactions/Problem3.sql
@@ -1,7 +1,8 @@
-CREATE OR ALTER PROC usp_DepositMoney(@accountId INT, @MoneyAmount MONEY)
+CREATE PROC usp_DepositMoney(@accountId INT, @MoneyAmount MONEY)
 AS
 	BEGIN TRANSACTION
 		
+    BEGIN TRY
 		IF(@MoneyAmount <= 0)
 			BEGIN
 				THROW 50010, 'Amount shound be positive.', 1;
@@ -13,12 +14,14 @@ AS
 			BEGIN
 				THROW 50011, 'Account does not exist.', 1;
 			END
-	
+	END TRY
+    BEGIN CATCH
+    	PRINT ERROR_MESSAGE();
+    END CATCH
 	UPDATE Accounts 
 		SET Balance += @MoneyAmount WHERE Accounts.Id = @accountId;
 
-		COMMIT
-
+	COMMIT
 GO
 
 SELECT * FROM Accounts
