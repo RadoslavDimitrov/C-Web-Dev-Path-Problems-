d17e7b3057369ddf52757fb60ed640a6a5dc86ad
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-02-08 22:36:15 +0200

    Update

--- C#DataBase/EntityFrameworkCore/EntityFrameworkIntro/EntityFrameworkIntro/EntityFrameworkIntro/StartUp.cs
+++ C#DataBase/EntityFrameworkCore/EntityFrameworkIntro/EntityFrameworkIntro/EntityFrameworkIntro/StartUp.cs
@@ -14,7 +14,47 @@ namespace EntityFrameworkIntro
         {
             SoftUniContext DBcontext = new SoftUniContext();
 
-            Console.WriteLine(GetEmployee147(DBcontext));
+            Console.WriteLine(GetDepartmentsWithMoreThan5Employees(DBcontext));
+        }
+
+        //Problem 10
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var departments = context.Departments
+                 .Where(d => d.Employees.Count > 5)
+                 .OrderBy(d => d.Employees.Count)
+                 .ThenBy(d => d.Name)
+                 .Select(d => new
+                 {
+                     DepartmentName = d.Name,
+                     ManagerFirstName = d.Manager.FirstName,
+                     ManagerLastName = d.Manager.LastName,
+                     Employees = d.Employees.Select(e => new
+                     {
+                         e.FirstName,
+                         e.LastName,
+                         e.JobTitle
+                     })
+                     .OrderBy(e => e.FirstName)
+                     .ThenBy(e => e.LastName)
+                     .ToList()
+                 })
+                 .ToList();
+
+            foreach (var d in departments)
+            {
+                sb.AppendLine($"{d.DepartmentName} - {d.ManagerFirstName} {d.ManagerLastName}");
+
+                foreach (var e in d.Employees)
+                {
+                    sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
         }
 
         //Problem 9
