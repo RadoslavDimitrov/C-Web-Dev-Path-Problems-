70420bc8c4687cdb6206e199b1b21174af00184b
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-02-23 23:15:09 +0200

    update

--- C#DataBase/EntityFrameworkCore/AdvancedQuerying/BookShop/StartUp.cs
+++ C#DataBase/EntityFrameworkCore/AdvancedQuerying/BookShop/StartUp.cs
@@ -16,17 +16,59 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-
+            //Problem 2
             //var input = Console.ReadLine();
             //string output = GetBooksByAgeRestriction(db, input);
 
             //Console.WriteLine(output);
 
-            Console.WriteLine(GetGoldenBooks(db));
+
+            //Problem 3
+            //Console.WriteLine(GetGoldenBooks(db));
+
+
+            //Problem 4
+            //Console.WriteLine(GetBooksByPrice(db));
 
         }
 
+        //Problem 5. Not Released In
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var books = context.Books
+                .AsEnumerable()
+                .Where(b => b)
+
+            return null;
+        }
+
         //Problem 4 -Books by Price
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var sb = new StringBuilder();
+
+            var books = context
+                .Books
+                .Select(b => new
+                {
+                    Title = b.Title,
+                    Price = b.Price
+                })
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - " +
+                              $"${book.Price:f2}");
+            }
+
+
+            return sb.ToString().Trim();
+        }
 
         //Problem 3 -Golden Books
 
