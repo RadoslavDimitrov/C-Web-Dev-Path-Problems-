95b2b92401f2a310817317bf1678774244c2911d
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2021-03-03 00:05:37 +0200

    Update

--- C#DataBase/EntityFrameworkCore/JsonProcessing/ProductShop/StartUp.cs
+++ C#DataBase/EntityFrameworkCore/JsonProcessing/ProductShop/StartUp.cs
@@ -13,22 +13,53 @@ namespace ProductShop
         private static string ResultsDirectoryPath = "../../../Datasets/Results";
         public static void Main(string[] args)
         {
-            //string input = File.ReadAllText("users.json");
+            ProductShopContext context = new ProductShopContext();
 
-            //string result = ImportUsers()
+
+            //Problem 2
+            string input = File.ReadAllText("../../../Datasets/products.json");
+
+            string result = ImportProducts(context, input);
+
+            Console.WriteLine(result);
+
+            //Problem 1
+            //string input = File.ReadAllText("../../../Datasets/users.json");
+
+            //string result = ImportUsers(context, input);
+
+            //Console.WriteLine(result);
         }
 
-        //public static string ImportUsers(ProductShopContext context, string inputJson)
-        //{
-        //    List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+        //Problem 3
 
-        //    context.Users.AddRange(users);
 
-        //    int count = users.Count;
+        //Problem 2
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
 
-        //    context.SaveChanges();
+            context.Products.AddRange(products);
 
-        //    return $"Successfully imported {count}";
-        //}
+            int count = products.Count;
+
+            context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+
+        //problem 1
+        public static string ImportUsers(ProductShopContext context, string inputjson)
+        {
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputjson);
+
+            context.Users.AddRange(users);
+
+            int count = users.Count;
+
+            context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
     }
 }
